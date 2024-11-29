267d2eea3aac101dfd355a2079476d6a0b4cde1c
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-11-29 16:16:19 -0700

    Console log a given location's temp

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # weather
+Test for using api
---
+++ script.js
@@ -0,0 +1,51 @@
+function getUrl(location) {
+    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location.toLowerCase() + "?key=AG7GMXU4EJGMMMJ7ZM74KJ4GB";
+    return url;
+}
+
+function getResponse(url) {
+    fetch(url, {
+        mode: 'cors'
+    })
+        .then(function (response) {
+            return response.json(); //process response
+        })
+        .then(function(data) {
+            console.log(data.currentConditions.temp); // use the processed data to get temp
+        })
+        .catch(function(err) {
+            console.log("ERROR"); // handle error
+        });
+}
+const formContainer = document.querySelector("#formContainer");
+const locationBtn = document.querySelector("#locationBtn");
+locationBtn.addEventListener("click", () => {
+    if (formContainer.children.length == 0) {
+        const newForm = document.createElement("form");
+        const label = document.createElement("label");
+        label.setAttribute("for", "location");
+        label.textContent = "Location";
+        const input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("id", "location")
+        input.setAttribute("name", "location");
+        input.setAttribute("placeholder", "London");
+        newForm.appendChild(label);
+        newForm.appendChild(input);
+
+        const submitBtn = document.createElement("input");
+        submitBtn.setAttribute("type", "submit");
+        submitBtn.setAttribute("value", "Enter Location");
+        newForm.appendChild(submitBtn);
+
+        formContainer.appendChild(newForm);
+        newForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            const location = input.value;
+            const url = getUrl(location);
+            const response = getResponse(url);
+            this.remove();
+        })
+    }
+})
+
