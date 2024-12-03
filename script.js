400f3f7b0c5e9d613b725178a991ed7bd2c00c9d
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-12-02 17:08:00 -0700

    Now displays the current condition and a gif of the condition

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function getUrl(location) {
-    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location.toLowerCase() + "?key=AG7GMXU4EJGMMMJ7ZM74KJ4GB";
-    return url;
+function getUrl(type, searchQuery) {
+    if (type == "weather") {
+        const location = searchQuery;
+        const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location.toLowerCase() + "?key=AG7GMXU4EJGMMMJ7ZM74KJ4GB";
+        return url;
+    }
+
+    if (type == "giphy") {
+        const gif = searchQuery;
+        const url = "https://api.giphy.com/v1/gifs/translate?api_key=DVNRz1CLadpeh4Xa3H0DAxrBeLdGsDaq&s=" + gif.toLowerCase();
+        return url;
+    }
 }
 
 function getResponse(url) {
@@ -11,19 +20,29 @@ function getResponse(url) {
             return response.json(); //process response
         })
         .then(function (data) {
-            return data.currentConditions; // use the processed data to get currentConditions
+            return data; // return the processed data
         })
         .catch(function (err) {
             console.log("ERROR"); // handle error
         });
 }
 
-function displayWeather(currentConditions) {
+function displayWeather(weatherData) {
     const container = document.querySelector("#container");
     const tempDiv = document.createElement("div");
-    tempDiv.textContent = currentConditions.temp;
-    console.log(currentConditions)
-    const conditionDiv = document.createElement("img");
+    tempDiv.textContent = weatherData.currentConditions.temp;
+    const conditionDiv = document.createElement("div");
+    const conditionImg = document.createElement("img");
+    conditionDiv.textContent = weatherData.currentConditions.conditions;
+    const conditionIcon = weatherData.currentConditions.icon;
+    const conditionIconFiltered = conditionIcon.replace(/-/g, "").toLowerCase() // remove all hyphens and send to lower case (just in case)
+    const typeGiphy = "giphy";
+    const urlGiphy = getUrl(typeGiphy, conditionIconFiltered);
+    getResponse(urlGiphy)
+        .then((data) => {
+            conditionImg.src = data.data.images.original.url;
+        })
+    conditionDiv.appendChild(conditionImg);
     container.appendChild(tempDiv);
     container.appendChild(conditionDiv);
 }
@@ -52,13 +71,14 @@ locationBtn.addEventListener("click", () => {
         formContainer.appendChild(newForm);
         newForm.addEventListener("submit", function (event) {
             event.preventDefault();
-            const location = input.value;
-            const url = getUrl(location);
-            let currentConditions = "";
-            getResponse(url)
+            const userLocation = input.value;
+            const typeWeather = "weather";
+            const urlWeather = getUrl(typeWeather, userLocation);
+            let weatherData = "";
+            getResponse(urlWeather)
                 .then((data) => {
-                    currentConditions = data;
-                    displayWeather(currentConditions);
+                    weatherData = data;
+                    displayWeather(weatherData);
                 });
             this.remove();
         })
